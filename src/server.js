737d5619de557eadef02748cbfe4a5fafded3076
MaxMEllon <maxmellon1994@gmail.com>
2018-02-25 14:35:19 +0900

    Use my profile

--- src/server.js
+++ src/server.js
@@ -5,6 +5,9 @@ const io = require('socket.io')(http)
 const dev = process.env.NODE_ENV === 'development'
 
 io.on('connection', socket => {
+  socket.send('socket/connected', {
+    message: '接続しました'
+  })
 })
 
 http.listen(8080, () => {
